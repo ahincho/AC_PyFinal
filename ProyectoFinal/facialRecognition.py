02b05a19c8ed0de01d791be2a7ef575119458c7a
Angel Hincho <82984150+ahincho@users.noreply.github.com>
2021-11-27 20:50:54 -0500

    Agregando botones del Registro e Inicio de Sesion

--- ProyectoFinal/facialRecognition.py
+++ ProyectoFinal/facialRecognition.py
@@ -3,16 +3,25 @@ from tkinter import *
 
 from tkinter import messagebox as msg
 
-# Variables Globales para el tamanio y letra
+# Variables Globales para el Tamanio y Fuente
 
-sizeScreen = "500x250"
+sizeScreen = "400x235"
 fontLabel = "Century Gothic"
+
+# Variables Globales para algunos Colores
+
+colorWhite = "#f4f5f4"
+colorBlack = "#101010"
 
 mainScreen = Tk()
 mainScreen.geometry(sizeScreen)
 mainScreen.title("Sistema de Reconocimiento Facial")
-mainScreen.configure(bg = "#151515")
+mainScreen.configure(bg = "#101010")
 
-Label(text = "¡Bienvenido!", fg = "#f4f5f4", bg = "#101010", font = (fontLabel, 18), width = "500", height = "2").pack()
+Label(text = "¡Bienvenido!", fg = colorWhite, bg = colorBlack, font = (fontLabel, 18), width = "500", height = "2").pack()
+
+Button(text = "Iniciar Sesion", fg = colorWhite, bg = "#202020", activebackground = "#151515", borderwidth = 0, font = (fontLabel, 16), width = "40", command = "").pack()
+
+Button(text = "Registrarse", fg = colorWhite, bg = "#202020", activebackground = "#151515", borderwidth = 0, font = (fontLabel, 16), width = "40", command = "").pack()
 
 mainScreen.mainloop()
