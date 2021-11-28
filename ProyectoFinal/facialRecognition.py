6a0223f52a2c33a9c163bd031f62e1090bcaec61
Angel Hincho <82984150+ahincho@users.noreply.github.com>
2021-11-28 10:50:09 -0500

    Agregando Formularios para Registrarse e Iniciar Sesion

--- ProyectoFinal/facialRecognition.py
+++ ProyectoFinal/facialRecognition.py
@@ -40,8 +40,10 @@ def register():
 
     # Creamos la nueva pantalla a la 'altura' de la Principal
     screen1 = Toplevel(mainScreen)
+    user1 = StringVar()
 
     newScreen(screen1, "Registrarse")
+    userEntry1 = formConfig(screen1, user1, 0)
 
 # Metodo usado por el Comando o Accion 'Login', se activara al
 # cliquear el Boton 'Iniciar Sesion' de la pantalla principal
@@ -52,9 +54,29 @@ def login():
     global screen2
 
     # Creamos la nueva pantalla a la 'altura' de la Principal
-    screen1 = Toplevel(mainScreen)
+    screen2 = Toplevel(mainScreen)
+    user2 = StringVar()
 
-    newScreen(screen1, "Iniciar Sesion")
+    newScreen(screen2, "Iniciar Sesion")
+    userEntry2 = formConfig(screen2, user2, 1)
+
+# Agregar los Labels necesarios para el Registro o Inicio de Sesion
+# Diferenciamos si es Registro o Login por la variable 'flag'
+
+def formConfig(screen, user, flag):
+    Label(screen, text = "Usuario:", fg = colorWhite, bg = colorBackground, font = (fontLabel, 16)).pack()
+    entry = Entry(screen, textvariable = user, justify = CENTER, font = (fontLabel, 16))
+    entry.focus_force()
+    entry.pack(side = TOP, ipadx = 30, ipady = 6)
+
+    getEnter(screen)
+
+    if flag:
+        Button(screen, text = "Capturar Rostro", fg = colorWhite, bg = colorButton, activebackground = colorBackground, borderwidth = 0, font = (fontLabel, 16), height="2", width="40", command = "").pack()
+    else:
+        Button(screen, text = "Capturar Rostro", fg = colorWhite, bg = colorButton, activebackground = colorBackground, borderwidth = 0, font = (fontLabel, 16), height="2", width="40", command = "").pack()
+    
+    return entry
 
 # Metodo Principal o Main del Programa
 
