916d503e6fb94d6c472fa448cf8d578eff8a71ce
Angel Hincho <82984150+ahincho@users.noreply.github.com>
2021-11-27 21:01:45 -0500

    Insertando saltos de lineas entre los botones

--- ProyectoFinal/facialRecognition.py
+++ ProyectoFinal/facialRecognition.py
@@ -13,15 +13,32 @@ fontLabel = "Century Gothic"
 colorWhite = "#f4f5f4"
 colorBlack = "#101010"
 
+# Variables Globales para algunos Colores de Elementos
+
+colorBackground = "#151515"
+colorButton = "#303030"
+
+# Metodo Auxiliar para insertar un Espacio o Salto de Linea
+# El Metodo recibe una pantalla para aplicar un Salto de Linea
+
+def getEnter(screen):
+    Label(screen, text = "", bg = colorBackground).pack()
+
+# Metodo Principal o Main del Programa
+
 mainScreen = Tk()
 mainScreen.geometry(sizeScreen)
 mainScreen.title("Sistema de Reconocimiento Facial")
-mainScreen.configure(bg = "#101010")
+mainScreen.configure(bg = colorBackground)
 
 Label(text = "¡Bienvenido!", fg = colorWhite, bg = colorBlack, font = (fontLabel, 18), width = "500", height = "2").pack()
 
-Button(text = "Iniciar Sesion", fg = colorWhite, bg = "#202020", activebackground = "#151515", borderwidth = 0, font = (fontLabel, 16), width = "40", command = "").pack()
+getEnter(mainScreen)
 
-Button(text = "Registrarse", fg = colorWhite, bg = "#202020", activebackground = "#151515", borderwidth = 0, font = (fontLabel, 16), width = "40", command = "").pack()
+Button(text = "Iniciar Sesion", fg = colorWhite, bg = colorButton, activebackground = colorBackground, borderwidth = 0, font = (fontLabel, 16), width = "40", command = "").pack()
+
+getEnter(mainScreen)
+
+Button(text = "Registrarse", fg = colorWhite, bg = colorButton, activebackground = colorBackground, borderwidth = 0, font = (fontLabel, 16), width = "40", command = "").pack()
 
 mainScreen.mainloop()
