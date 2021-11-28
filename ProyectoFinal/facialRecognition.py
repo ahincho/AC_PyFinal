2257dc991f30c6d632d290f371be3a5bd4a960b1
Angel Hincho <82984150+ahincho@users.noreply.github.com>
2021-11-27 20:20:06 -0500

    Creando GUI con la libreria Tkinter

---
+++ ProyectoFinal/facialRecognition.py
@@ -0,0 +1,15 @@
+
+from tkinter import *
+
+from tkinter import messagebox as msg
+
+# Variables Globales para el tamanio y letra
+
+sizeScreen = "500x250"
+fontLabel = "Century Gothic"
+
+generalScreen = Tk()
+generalScreen.geometry(sizeScreen)
+generalScreen.title("Sistema de Reconocimiento Facial")
+
+generalScreen.mainloop()
